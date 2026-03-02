f8414810d81cf7e7a8c601f57eed619fac6c56ad
Bouchentouf Omar <bouchentoufomar05@gmail.com>
2026-03-03 00:20:42 +0100

    review

--- sql/testing.sql
+++ sql/testing.sql
@@ -1,40 +1,36 @@
 USE olist_ecommerce;
 /*growth and Revenue Analysis*/
-SELECT SUM(price + freight_value) AS total_revenue
-FROM order_items;
+
+/*total revenue generated from delivered products by the platform*/
+SELECT SUM(oi.price + oi.freight_value) AS total_revenue
+FROM orders AS o
+INNER JOIN order_items AS oi
+ON o.order_id = oi.order_id
+WHERE o.order_status = "delivered";
 
 /*Yearly generated revenue*/
-SELECT YEAR(o.order_purchase_timestamp) AS year, SUM(price + freight_value) AS total_revenue
-FROM order_items AS oi
-INNER JOIN orders AS o
-ON oi.order_id = o.order_id
-WHERE o.order_status = 'delivered'
-GROUP BY YEAR(o.order_purchase_timestamp) ORDER BY year ASC;
-
-/*Monthly generated revenue over all the years (year-month-revenue)*/
-SELECT DATE_FORMAT(o.order_purchase_timestamp, '%Y-%m') AS month, SUM(oi.price + oi.freight_value) AS total_revenue
+SELECT YEAR(o.order_delivered_customer_date) AS year, ROUND(SUM(oi.price + oi.freight_value),2) AS total_value
 FROM orders AS o
 INNER JOIN order_items AS oi
 ON o.order_id = oi.order_id
-WHERE order_status = "delivered"
-GROUP BY month
-ORDER BY month ASC;
+WHERE o.order_status = "delivered" AND o.order_delivered_customer_date > 0
+GROUP BY year
+ORDER BY year;
 
 /*growth percentage month over month for delivered products only*/
-WITH monthly_revenue AS(
-SELECT DATE_FORMAT(o.order_purchase_timestamp, '%Y-%m') AS y_month, 
-       SUM(oi.price + oi.freight_value) AS total_revenue
-FROM orders AS o
-INNER JOIN order_items AS oi
-ON o.order_id = oi.order_id
-WHERE o.order_status = "delivered"
-GROUP BY y_month
+WITH year_month_table AS(
+	SELECT DATE_FORMAT(o.order_delivered_customer_date, '%Y-%m') AS y_month,
+		   ROUND(SUM(price + freight_value),2) AS total_revenue
+	FROM orders AS o
+	INNER JOIN order_items AS oi
+	ON o.order_id = oi.order_id
+	WHERE order_status = 'delivered' AND YEAR(o.order_delivered_customer_date) > 0
+	GROUP BY y_month	
 )
-SELECT y_month,
-       LAG(total_revenue,1) OVER (ORDER BY y_month) AS last_month_revenue,
+SELECT y_month, 
+	   LAG(total_revenue) OVER (ORDER BY y_month) AS last_month_revenue, 
        total_revenue,
-       total_revenue - LAG(total_revenue,1) OVER(ORDER BY y_month) AS monthly_revenue_differance,
-       ROUND((total_revenue - LAG(total_revenue,1) OVER(ORDER BY y_month))/ NULLIF(LAG(total_revenue) OVER (ORDER BY y_month), 0)
-       * 100, 2) AS growth_percentage
-FROM monthly_revenue
-ORDER BY y_month ASC;
+       ROUND((total_revenue - LAG(total_revenue) OVER(ORDER BY y_month))/LAG(total_revenue) OVER(ORDER BY y_month)*100,2) AS growth_percentage
+FROM year_month_table
+ORDER BY y_month;
+
